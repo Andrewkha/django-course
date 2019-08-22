347125dd320cf6225df079175724fb03a1efe8b1
Chernyshov, Andrey <andrey.chernyshov@intel.com>
2019-08-22 19:28:20 +0300

    low level rendering

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Bb
 # Create your views here
 
 
 def index(request):
-    s = 'List of publications\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('published'):    # minus stands for desc order
-        s += bb.published.strftime('%d%m%Y %H-%M') + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('published')    # minus stands for desc order
+    context = {'bbs': bbs}
 
-    return HttpResponse(s, content_type='text/plain', charset='utf-8')
+    return HttpResponse(template.render(context, request))
 
