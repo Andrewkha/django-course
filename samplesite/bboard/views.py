3e9c28dcffc49f7e6cbb401f8f68647c5c681dfb
Chernyshov, Andrey <andrey.chernyshov@intel.com>
2019-08-22 19:04:40 +0300

    getting list of models added

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bb
 # Create your views here
 
 
 def index(request):
-    return HttpResponse('Here the list goes...')
+    s = 'List of publications\r\n\r\n\r\n'
+    for bb in Bb.objects.order_by('published'):    # minus stands for desc order
+        s += bb.published.strftime('%d%m%Y %H-%M') + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
+
+    return HttpResponse(s, content_type='text/plain', charset='utf-8')
+
